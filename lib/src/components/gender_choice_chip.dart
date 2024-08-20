a6d320ee95c2ef88e86e3c360aea0a5d17a55581
fofack <74138855+fofack@users.noreply.github.com>
2024-08-20 17:15:35 +0100

    ticket-2/create_profile_step_1o1 : Adding the interface for step 1 of profile creation (#2)

---
+++ lib/src/components/gender_choice_chip.dart
@@ -0,0 +1,60 @@
+import 'package:featlink_app/src/config/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class GenderChoiceChip extends StatelessWidget {
+  final String text;
+  final double? fontSize;
+  final Size fixedSize;
+  final Color selectedColor;
+  final Color borderColor;
+  final Color textColor;
+  final String selectedValue;
+  final void Function(String) onSelected;
+
+  const GenderChoiceChip({
+    required this.text,
+    required this.selectedValue,
+    required this.onSelected,
+    super.key,
+    this.fontSize,
+    this.fixedSize = const Size.fromHeight(45),
+    this.selectedColor = AppColors.primary,
+    this.borderColor = AppColors.primary,
+    this.textColor = Colors.black,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ChoiceChip(
+      label: SizedBox(
+        width: 100,
+        child: Align(
+          alignment: AlignmentDirectional.center,
+          child: Text(
+            text,
+            style: TextStyle(
+              fontSize: fontSize,
+              fontWeight: FontWeight.bold,
+              color: textColor,
+            ),
+          ),
+        ),
+      ),
+      selected: selectedValue == text,
+      selectedColor: selectedColor,
+      onSelected: (bool selected) {
+        if (selected) {
+          onSelected(text);
+        }
+      },
+      shape: RoundedRectangleBorder(
+        side: BorderSide(
+          color: borderColor,
+          width: 2.0,
+        ),
+        borderRadius: const BorderRadius.all(Radius.circular(20)),
+      ),
+      showCheckmark: false,
+    );
+  }
+}
--- lib/src/resources/app_assets.dart
+++ lib/src/resources/app_assets.dart
@@ -2,4 +2,6 @@ final class AppAssets {
   const AppAssets._();
 
   static const imageLogo = 'assets/images/logo.png';
+  static const contractImage = 'assets/images/contract.png';
+  static const profileImage = 'assets/images/profile_image.png';
 }
