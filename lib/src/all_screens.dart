3349f4c28e94744c912a9c8377f37f6b2db897bd
divinjordan <nguekeu3divin@gmail.com>
2024-08-24 15:49:02 +0100

    update

--- lib/src/all_screens.dart
+++ lib/src/all_screens.dart
@@ -8,13 +8,10 @@ import 'screens/_create_profile/create_profile_step_1o1_screen.dart';
 import 'screens/_create_profile/create_profile_step_1o3_screen.dart';
 import 'screens/_auth/login_screen.dart';
 import 'screens/_auth/register_screen.dart';
-import 'screens/_create_profile/create_profile_step_1o3_screen.dart';
 import 'screens/_create_profile/create_profile_step_1o4_screen.dart';
 import 'screens/_create_profile/create_profile_step_1o6_screen.dart';
 import 'screens/_create_profile/create_profile_step_1o7_screen.dart';
 import 'screens/_create_profile/create_profile_step_1o8_screen.dart';
-import 'screens/_create_profile/create_profile_step_1o9_screen/create_profile_step_1o9_screen.dart';
-import 'screens/_create_profile/create_profile_step_2o1_screen.dart';
 
 /// Le test unitaire va chercher toute les pages du dossier screens pour se
 /// rassurer qu'il s'affiche sans erreur, seulement pour chaque fichier,
