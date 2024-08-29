9d1ba4e14de31c44927f7852c070e17e36b8aa67
bertho <berthoazeye21@mail.com>
2024-08-29 12:16:55 +0100

    ticket-22/crush-page- add crush page

--- lib/src/resources/app_assets.dart
+++ lib/src/resources/app_assets.dart
@@ -4,7 +4,6 @@ final class AppAssets {
   static const imageLogo = 'assets/images/logo.png';
   static const contractImage = 'assets/images/contract.png';
   static const profileImage = 'assets/images/profile_image.png';
-  static const fakeAvatarImage = 'assets/images/fake_avatar.jpg';
 
   static const sizeIcon = 'assets/images/size.png';
   static const weightIcon = 'assets/images/weight.png';
