4af9c668eb90e402dc541022da817b66f459a889
BogneStanley <63175160+BogneStanley@users.noreply.github.com>
2024-09-03 15:20:34 +0100

    ticker-32/parametres-mode_prive_vip: private mode settings screen (#37)

--- lib/src/all_screens.dart
+++ lib/src/all_screens.dart
@@ -8,6 +8,7 @@ import 'screens/_create_profile/create_profile_step_2o2_screen/create_profile_st
 import 'screens/_settings/unsubscribe_screen.dart';
 import 'screens/_discovery/discovery_search_1_screen.dart';
 import 'screens/_discovery/discovery_search_2_screen.dart';
+import 'screens/_settings/private_mode_settings_screen.dart';
 import 'screens/_swipe/filter_screen/filter_screen.dart';
 import 'screens/welcome2_screen/welcome2_screen.dart';
 import 'screens/welcome_screen.dart';
@@ -71,6 +72,7 @@ final Map<String, Widget Function()> allScreens = {
   'edit_profile_interet_screen.dart': () => EditProfileInteretScreen(),
 
   'd_day_zone_screen.dart': () => const DDayZoneScreen(),
+  'private_mode_settings_screen.dart': () => const PrivateModeSettingsScreen(),
 
   // Ajoutez d'autres chemins de fichiers ici
 };
