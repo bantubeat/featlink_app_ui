029e355c31859694ed20e451c864e7fb769fdebf
bertho <berthoazeye21@mail.com>
2024-09-07 20:39:54 +0100

    ticket-25/edit_profile_user_side add pages

--- lib/src/screens/_edit_profile/edit_profile_user_side_screen/edit_profile_user_side_screen.dart
+++ lib/src/screens/_edit_profile/edit_profile_user_side_screen/edit_profile_user_side_screen.dart
@@ -4,7 +4,7 @@ import 'package:featlink_app/src/components/app_navigation_bottom_sheet.dart';
 import 'package:featlink_app/src/components/primary_button.dart';
 import 'package:featlink_app/src/config/app_colors.dart';
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 import 'widgets/select_widget.dart';
 
 class EditProfileUserSideScreen extends StatefulWidget {
@@ -14,71 +14,197 @@ class EditProfileUserSideScreen extends StatefulWidget {
 }
 
 class _EditProfileUserSideScreen extends State<EditProfileUserSideScreen> {
+  List<String> choices = [
+    'News',
+    'Entertainment',
+    'Politics',
+    'Automotive',
+    'Sports',
+    'Education',
+    'Fashion',
+    'Travel',
+    'Food',
+    'Tech',
+    'Science',
+    'Arts',
+  ];
+
+  void setSelectedValue(String? value) {
+    setState(() => selectedValue = value);
+  }
+
+  String? selectedValue = 'News';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 10,
-          ),
-          Wrap(
-            spacing: 40,
-            alignment: WrapAlignment.spaceAround,
-            children: [
-              InkWell(
-                onTap: () {},
-                child: Text(
-                  LocaleKeys.edit_profile_user_side_screen_data.tr(),
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 14,
-                    color: AppColors.myDark,
-                  ),
-                ),
-              ),
-              InkWell(
-                onTap: () {},
-                child: Text(
-                  LocaleKeys.edit_profile_user_side_screen_hooks.tr(),
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 14,
-                    color: AppColors.primary,
-                  ),
-                ),
-              ),
-              InkWell(
-                onTap: () {},
-                child: Text(
-                  LocaleKeys.edit_profile_user_side_screen_interests.tr(),
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 14,
-                    color: AppColors.myDark,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          // SelectWidget(),
+    List<Choise> choicesList = [
+      Choise(
+        label: 'Ville',
+        choices: choices,
+        id: 0,
+        selectedValue: 'Douala',
+        isCenter: true,
+      ),
+      Choise(
+        label: 'Taille',
+        choices: choices,
+        id: 0,
+        selectedValue: '(151cm-199cm)',
+        isCenter: false,
+      ),
+      Choise(
+        label: 'Poids',
+        choices: choices,
+        id: 0,
+        selectedValue: '(60kg à 90kg)',
+        isCenter: false,
+      ),
+      Choise(
+        label: 'Enfants',
+        choices: choices,
+        id: 0,
+        selectedValue: 'Oui',
+        isCenter: false,
+      ),
+      Choise(
+        label: 'Cigarette',
+        choices: choices,
+        id: 0,
+        selectedValue: 'Oui',
+        isCenter: false,
+      ),
+      Choise(
+        label: 'Alcool',
+        choices: choices,
+        id: 0,
+        selectedValue: 'En soirée',
+        isCenter: false,
+      ),
+      Choise(
+        label: 'Recherche',
+        choices: choices,
+        id: 0,
+        selectedValue: 'Amitié, relation libre',
+        isCenter: false,
+      ),
+      Choise(
+        label: 'Diplôme',
+        choices: choices,
+        id: 0,
+        selectedValue: 'Aucun',
+        isCenter: false,
+      ),
+      Choise(
+        label: 'Vie',
+        choices: choices,
+        id: 0,
+        selectedValue: 'Seul',
+        isCenter: false,
+      ),
+      Choise(
+        label: 'Préf.Sexuel',
+        choices: choices,
+        id: 0,
+        selectedValue: 'Hétéro',
+        isCenter: false,
+      ),
+      Choise(
+        label: 'Type Ethnique',
+        choices: choices,
+        id: 0,
+        selectedValue: 'Africaine',
+        isCenter: false,
+      ),
+      Choise(
+        label: 'Réligion',
+        choices: choices,
+        id: 0,
+        selectedValue: 'Islam',
+        isCenter: false,
+      ),
+    ];
 
-          const SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-            child: PrimaryButton(
-              onPressed: (_) {},
-              fontSize: 19,
-              fontWeight: FontWeight.bold,
-              text: LocaleKeys.edit_profile_user_side_screen_save.tr(),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 10,
             ),
-          ),
-          const SizedBox(
-            height: 110,
-          ),
-        ],
+            Wrap(
+              spacing: 40,
+              alignment: WrapAlignment.spaceAround,
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: Text(
+                    LocaleKeys.edit_profile_user_side_screen_data.tr(),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                      color: AppColors.primary,
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Text(
+                    LocaleKeys.edit_profile_user_side_screen_hooks.tr(),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                      color: AppColors.myDark,
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Text(
+                    LocaleKeys.edit_profile_user_side_screen_interests.tr(),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                      color: AppColors.myDark,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 10,
+              ),
+              child: Text(
+                'Homme',
+                style: GoogleFonts.inter(
+                  color: const Color.fromRGBO(101, 110, 123, 1),
+                  fontWeight: FontWeight.w800,
+                  fontSize: 19,
+                ),
+              ),
+            ),
+            for (int i = 0; i < choicesList.length; i++)
+              SelectWidget(
+                item: choicesList[i],
+                setSelectedValue: setSelectedValue,
+              ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              child: PrimaryButton(
+                onPressed: (_) {},
+                fontSize: 19,
+                fontWeight: FontWeight.bold,
+                text: LocaleKeys.edit_profile_user_side_screen_save.tr(),
+              ),
+            ),
+            const SizedBox(
+              height: 110,
+            ),
+          ],
+        ),
       ),
       floatingActionButton: const AppNavigationBottomSheet(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
--- lib/src/screens/_edit_profile/edit_profile_user_side_screen/widgets/select_widget.dart
+++ lib/src/screens/_edit_profile/edit_profile_user_side_screen/widgets/select_widget.dart
@@ -1,110 +1,174 @@
+import 'package:choice/choice.dart';
+import 'package:featlink_app/src/config/app_colors.dart';
 import 'package:flutter/material.dart';
-import 'package:multi_select_flutter/multi_select_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class Animal {
+class Choise {
   final int id;
-  final String name;
+  final String? selectedValue;
+  final String label;
+  final bool isCenter;
+  final List<dynamic> choices;
 
-  Animal({
+  Choise({
     required this.id,
-    required this.name,
+    required this.choices,
+    required this.label,
+    required this.selectedValue,
+    required this.isCenter,
   });
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({required this.title, super.key});
-  final String title;
+class SelectWidget extends StatelessWidget {
+  const SelectWidget({
+    required this.item,
+    required this.setSelectedValue,
+    this.bottomSwichtText,
+    super.key,
+  });
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  final String? bottomSwichtText;
 
-class _MyHomePageState extends State<MyHomePage> {
-  static final List<Animal> _animals = [
-    Animal(id: 1, name: 'Lion'),
-    Animal(id: 2, name: 'Flamingo'),
-    Animal(id: 3, name: 'Hippo'),
-    Animal(id: 4, name: 'Horse'),
-    Animal(id: 5, name: 'Tiger'),
-    Animal(id: 6, name: 'Penguin'),
-    Animal(id: 7, name: 'Spider'),
-    Animal(id: 8, name: 'Snake'),
-    Animal(id: 9, name: 'Bear'),
-    Animal(id: 10, name: 'Beaver'),
-    Animal(id: 11, name: 'Cat'),
-    Animal(id: 12, name: 'Fish'),
-    Animal(id: 13, name: 'Rabbit'),
-    Animal(id: 14, name: 'Mouse'),
-    Animal(id: 15, name: 'Dog'),
-    Animal(id: 16, name: 'Zebra'),
-    Animal(id: 17, name: 'Cow'),
-    Animal(id: 18, name: 'Frog'),
-    Animal(id: 19, name: 'Blue Jay'),
-    Animal(id: 20, name: 'Moose'),
-    Animal(id: 21, name: 'Gecko'),
-    Animal(id: 22, name: 'Kangaroo'),
-    Animal(id: 23, name: 'Shark'),
-    Animal(id: 24, name: 'Crocodile'),
-    Animal(id: 25, name: 'Owl'),
-    Animal(id: 26, name: 'Dragonfly'),
-    Animal(id: 27, name: 'Dolphin'),
-  ];
-
-  final List<MultiSelectItem<Animal>> _items = _animals
-      .map((animal) => MultiSelectItem<Animal>(animal, animal.name))
-      .toList();
-
-  List<Animal> _selectedAnimals2 = [];
-  List<Animal> _selectedAnimals3 = [];
-  List<Animal> _selectedAnimals5 = [];
-  final _multiSelectKey = GlobalKey<FormFieldState>();
-
-  @override
-  void initState() {
-    super.initState();
-    _selectedAnimals5 = _animals;
-  }
+  final Choise item;
+  final void Function(String?) setSelectedValue;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            children: <Widget>[
-              const SizedBox(height: 40),
-              //################################################################################################
-              // Rounded blue MultiSelectDialogField
-              //################################################################################################
-              MultiSelectDialogField<Animal>(
-                items: _items,
-                title: const Text('Animals'),
-                selectedColor: Colors.blue,
+    return Column(
+      crossAxisAlignment:
+          item.isCenter ? CrossAxisAlignment.center : CrossAxisAlignment.start,
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 2,
+          ),
+          decoration: BoxDecoration(
+            color: AppColors.myGray,
+            borderRadius: BorderRadius.circular(3),
+          ),
+          child: Row(
+            children: [
+              Container(
+                width: 10,
+                height: 10,
                 decoration: BoxDecoration(
-                  color: Colors.blue.withOpacity(0.1),
-                  borderRadius: const BorderRadius.all(Radius.circular(40)),
-                  border: Border.all(color: Colors.blue, width: 2),
+                  color: AppColors.myWhite,
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                buttonIcon: const Icon(Icons.pets, color: Colors.blue),
-                buttonText: Text(
-                  'Favorite Animals',
-                  style: TextStyle(color: Colors.blue[800], fontSize: 16),
+              ),
+              const SizedBox(
+                width: 15,
+              ),
+              Text(
+                item.label,
+                style: GoogleFonts.inter(
+                  color: AppColors.myDark,
+                  fontWeight: FontWeight.w800,
+                  fontSize: 17,
                 ),
-                onConfirm: (results) {
-                  setState(() {
-                    _selectedAnimals5 = results;
-                  });
-                },
               ),
             ],
           ),
         ),
-      ),
+        SizedBox(
+          child: PromptedChoice<String>.single(
+            clearable: true,
+            searchable: true,
+            value: item.selectedValue,
+            onChanged: setSelectedValue,
+            itemCount: item.choices.length,
+            itemBuilder: (state, i) {
+              return RadioListTile(
+                value: item.choices[i],
+                groupValue: state.single,
+                onChanged: (value) {
+                  state.select(item.choices[i]);
+                },
+                title: ChoiceText(
+                  item.choices[i],
+                  highlight: state.search?.value,
+                ),
+              );
+            },
+            promptDelegate: ChoicePrompt.delegatePopupDialog(
+              maxHeightFactor: .7,
+              constraints: const BoxConstraints(maxWidth: 300),
+            ),
+            anchorBuilder: (context, selected) {
+              return InkWell(
+                onTap: selected,
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        item.selectedValue ?? 'Sélectionnez une option',
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const Icon(
+                        Icons.keyboard_arrow_down_outlined,
+                        color: AppColors.primary,
+                        size: 18,
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+            // anchorBuilder:("g"){
+            // 	return ChoiceAnchor.defaultLeadingIcon; // Change this to the desired icon for leading anchor
+            // }
+            // anchorBuilder: ChoiceAnchor.create(
+            //   inline: true,
+            //   leading: Padding(
+            //     padding: const EdgeInsets.all(8.0),
+            //     child: Icon(
+            //       Icons.delete, // Icône indiquant l'ouverture
+            //       color: Colors.grey.shade600,
+            //       size: 18,
+            //     ),
+            //   ),
+            //   trailing: const Padding(
+            //     padding: EdgeInsets.all(8.0),
+            //     child: Icon(
+            //       Icons
+            //           .keyboard_arrow_down_outlined, // Icône indiquant l'ouverture
+            //       color: AppColors.primary,
+            //       size: 18,
+            //     ),
+            //   ),
+            // ),
+            listBuilder:
+                ChoiceList.createVirtualized(padding: const EdgeInsets.all(20)),
+          ),
+        ),
+        if (bottomSwichtText != null)
+          Align(
+            child: Row(
+              children: [
+                Text(bottomSwichtText!),
+                Transform.scale(
+                  scale: 0.4,
+                  child: Switch(
+                    activeTrackColor: AppColors.bantubeatPrimary,
+                    activeColor: AppColors.myWhite,
+                    value: false,
+                    onChanged: (value) {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+      ],
     );
   }
 }
